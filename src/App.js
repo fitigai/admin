b166a2adc138688fd24ebb01842e0d95e279e867
Jeremy <rigaldo.jeremy@gmail.com>
2021-06-10 11:36:31 +0200

    add basics css files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React from "react"
+import { Button, Pane, Text, majorScale, BanCircleIcon, TickCircleIcon, WarningSignIcon, InfoSignIcon } from "evergreen-ui"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <aside background="gray900" className="app__aside">
+
+      </aside>
+      <header>
+        <Pane>
+          <BanCircleIcon color="danger" marginRight={16} />
+          <TickCircleIcon color="success" marginRight={16} />
+          <WarningSignIcon color="warning" marginRight={16} />
+          <InfoSignIcon color="info" marginRight={16} />
+        </Pane>
       </header>
+      <main>
+        <section>wfe</section>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
