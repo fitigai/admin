750e1fe2f67163c1d1c4a991ee9e0d3557ed9de8
Jeremy <rigaldo.jeremy@gmail.com>
2021-08-17 09:36:39 +0200

    add GridSystem

--- src/styles/Grid.js
+++ src/styles/Grid.js
@@ -1,97 +1,85 @@
 import React from "react"
 import styled, { css } from "styled-components"
 
-const center = styled.div`
+const Container = styled.main`
+  position: relative;
+  height: 100%;
+  display: block;
+  width: ${props => (props.narrowContainer ? "calc(100% - 1rem)" : "100%")};
+  max-width: 80rem;
+  margin: 0 auto;
+`
+
+export default function Container(props) {
+  return <Container>{props.children}</Container>
+}
+
+
+const FlexItems = {
+  top: `-webkit-box-align: flex-start;
+            -moz-box-align: flex-start;
+            -ms-flex-align: flex-start;
+            -webkit-align-items: flex-start;
+            align-items: flex-start;`,
+  stretch: `-webkit-box-align: stretch;
+            -moz-box-align: stretch;
+            -ms-flex-align: stretch;
+            -webkit-align-items: stretch;
+            align-items: stretch;`,
+  center: `-webkit-box-align: center;
+            -moz-box-align: center;
+            -ms-flex-align: center;
+            -webkit-align-items: center;
+            align-items: center;`
+}
+
+const Row = styled.div`
   display: -webkit-box;
   display: -moz-box;
   display: -ms-flexbox;
   display: -webkit-flex;
   display: flex;
-  -webkit-box-align: center;
-  -moz-box-align: center;
-  -ms-flex-align: center;
-  -webkit-align-items: center;
-  align-items: center;
   flex-wrap: wrap;
   justify-content: center;
+  align-items: ${FlexItems};
   width: 100%;
   height: 100%;
 `
 
-const top = styled.div`
-  display: -webkit-box;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: -webkit-flex;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  -webkit-box-align: flex-start;
-  -moz-box-align: flex-start;
-  -ms-flex-align: flex-start;
-  -webkit-align-items: flex-start;
-  align-items: flex-start;
-  align-content: stretch;
-  width: 100%;
-  height: 100%;
-`
-
-const stretch = styled.div`
-  display: -webkit-box;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: -webkit-flex;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  -webkit-box-align: stretch;
-  -moz-box-align: stretch;
-  -ms-flex-align: stretch;
-  -webkit-align-items: stretch;
-  align-items: stretch;
-  width: 100%;
-  height: 100%;
-`
-
-const alignRow = styled.div`
-  display: -webkit-box;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: -webkit-flex;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: ${props.align} || flex-start;
-  width: 100%;
-  height: 100%;
-`
 export default function Row(props) {
-  return <Row top stretch center>{props.children}</Row>
+  return <Row>{props.children}</Row>
 }
 
-function generateMobileGrid(columns) {
-  let createGrid = ''
-  for (let i = 0; i < columns; i += 1){
-    createGrid += `
-      @media ${DEVICE.mobileL} {
-        flex: 0 0 100% / ${columns} * ${i};
-        & > * {
-          padding: 0;
-          margin: 0;
-        }
-      }
-    `
-  }
-  return css`${createGrid}`;
+// const column = 12
+
+// function gridSystem(colNb) {
+//   let size = ''
+//   for (let i = 0; i < column; i += 1){
+//     size += `
+//       0 0 100% / ${column} * ${i};
+//     `
+//   }
+// }
+
+// const Col = styled.div`
+//    flex: ${gridSystem()}
+// `
+
+const media = {
+  xs: (styles) => {`
+    @media only screen and (min-width: 768px){
+      ${styles}
+    }
+  `}
 }
 
-const MobileLGrid = styled.div`
-  ${generateMobileGrid(12)};
+const Col = styled.div`
+   flex: ${(props) => props.size}
+   ${(props) => props.collapse && media[props.collapse](`
+      display: none;
+   `)}
 `
 
-export default function MobileGridContainer(props) {
-  return <MobileLGrid>{props.children}</MobileLGrid>
+export default function Col(props) {
+  return <Col>{props.children}</Col>
 }
-
-
-
